a69d59d98fb02c96bf6ea239c2df08f4df008f47
pmpgithublub <pmpgithub@gmail.com>
2020-06-06 17:48:04 +0300

    Correct database sql-script.

--- src/main/resources/sql/createDB.sql
+++ src/main/resources/sql/createDB.sql
@@ -51,6 +51,21 @@ CREATE TABLE `exhibits`
   COLLATE = utf8mb4_0900_ai_ci;
 
 
+CREATE TABLE `payments`
+(
+    `id`               bigint NOT NULL AUTO_INCREMENT,
+    `tickets_quantity` int    DEFAULT NULL,
+    `paid_date`        date   DEFAULT NULL,
+    `paid_sum`         bigint DEFAULT NULL,
+    `user_id`          bigint NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `FKUser_idx` (`user_id`),
+    CONSTRAINT `FKUser` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+
 CREATE TABLE `tickets`
 (
     `id`               bigint NOT NULL AUTO_INCREMENT,
@@ -70,21 +85,6 @@ CREATE TABLE `tickets`
     CONSTRAINT `FKHallId` FOREIGN KEY (`hall_id`) REFERENCES `halls` (`id`),
     CONSTRAINT `FKpaymemtId` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`id`),
     CONSTRAINT `FKUserId` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `payments`
-(
-    `id`               bigint NOT NULL AUTO_INCREMENT,
-    `tickets_quantity` int    DEFAULT NULL,
-    `paid_date`        date   DEFAULT NULL,
-    `paid_sum`         bigint DEFAULT NULL,
-    `user_id`          bigint NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `FKUser_idx` (`user_id`),
-    CONSTRAINT `FKUser` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
